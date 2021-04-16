a58636eb01e6fe3586e9ee75639a6897a60ae1ea
Mr. SAFONE <77989182+AsmSafone@users.noreply.github.com>
2021-04-17 00:38:14 +0600

    Update anime.py

--- bot/modules/anime.py
+++ bot/modules/anime.py
@@ -256,7 +256,7 @@ def weebhelp(update, context):
 • `/chr`*:* search character
 • `/mng`*:* search manga
 '''
-    update.effective_message.reply_photo("https://telegra.ph/file/db03910496f06094f1f7a.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_photo("https://telegra.ph/file/68ba9706cdf78f28b4a8c.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
 
 
 ANIME_HANDLER = CommandHandler("al", anime)
